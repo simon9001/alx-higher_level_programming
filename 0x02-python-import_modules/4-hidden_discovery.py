a3b4a5e84406fc0e7ec0f879c2e6c7b2e7ac61a3
Simon9001 <simongatungo300@gmail.com>
2023-06-23 09:30:02 +0000

    gito

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+# 4-hidden_discovery.py
+# Brennan D Baraban <375@holbertonschool.com>
+
 if __name__ == "__main__":
-  import hidden_4
-		array = dir(hidden_4)
-	for element in array:
-	if (element[:2] == "__"):
-	continue
-	print(element)
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
